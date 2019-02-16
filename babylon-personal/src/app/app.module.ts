4fc995df78ba58f4c25cd5f48385b6c31fefe4f8
Jose Stricklin <josestricklin@El-Diablo.local>
2019-02-16 16:17:20 -0700

    update with scss

--- babylon-personal/src/app/app.module.ts
+++ babylon-personal/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BgCanvasComponent } from './bg-canvas/bg-canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BgCanvasComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
