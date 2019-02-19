82f76a34f89288bf8102e69ad4e0d55a3240dccb
Jose Stricklin <josestricklin@El-Diablo.local>
2019-02-18 21:35:48 -0700

    update particles

--- babylon-personal/src/app/bg-canvas/bg-canvas.component.ts
+++ babylon-personal/src/app/bg-canvas/bg-canvas.component.ts
@@ -21,7 +21,7 @@ export class BgCanvasComponent implements OnInit {
         const scene = new BABYLON.Scene(this.engine);
         // Add a camera to the scene and attach it to the canvas
         const camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2.5, Math.PI / 6, 12, BABYLON.Vector3.Zero(), scene);
-        // camera.attachControl(this.canvas.nativeElement, true);
+        camera.attachControl(this.canvas.nativeElement, true);
 
         BABYLON.SceneLoader.Append(
             // '../../assets/scenes/grey-scene/',
@@ -54,7 +54,7 @@ export class BgCanvasComponent implements OnInit {
         });
     }
     particleInit(scene: BABYLON.Scene) {
-        const motes = new BABYLON.ParticleSystem('motes', 1000, scene);
+        const motes = new BABYLON.ParticleSystem('motes', 500, scene);
         motes.emitter = new BABYLON.Vector3(0, 0, 0);
         motes.minEmitBox = new BABYLON.Vector3(-5, -5, -5);
         motes.maxEmitBox = new BABYLON.Vector3(5, 5, 5);
@@ -63,26 +63,26 @@ export class BgCanvasComponent implements OnInit {
         // motes.color1 = new BABYLON.Color4(0.1, 0.1, 0.1, 0.1);
         // motes.colorDead = new BABYLON.Color4(0, 0, 0, 0);
         motes.addColorGradient(0, new BABYLON.Color4(0, 0, 0, 0));
-        motes.addColorGradient(0.5, new BABYLON.Color4(0.1, 0.1, 0.1, 1));
-        motes.addColorGradient(1, new BABYLON.Color4(0.1, 0.1, 0.1, 0.1));
+        motes.addColorGradient(0.5, new BABYLON.Color4(0.4, 0.4, 0.4, 1));
+        motes.addColorGradient(1, new BABYLON.Color4(0.1, 0.1, 0.1, 0.0));
         // motes.color1 = new BABYLON.Color4(0.3, 0.3, 1, 0.6);
         motes.maxLifeTime = 30;
-        motes.minLifeTime = 3;
+        motes.minLifeTime = 5;
 
         motes.direction1 = new BABYLON.Vector3(-7, 8, 3);
         motes.direction2 = new BABYLON.Vector3(7, -8, -3);
 
-        motes.emitRate = 50;
+        motes.emitRate = 10;
 
         // angular speed
         motes.minAngularSpeed = 0;
         motes.maxAngularSpeed = 0.5;
 
-        motes.minSize = 0.1;
-        motes.maxSize = 1;
+        motes.minSize = 0.05;
+        motes.maxSize = 0.5;
         motes.minEmitPower = .01;
         motes.maxEmitPower = .1;
-        motes.preWarmCycles = 500;
+        motes.preWarmCycles = 1000;
         motes.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
         motes.start();
 
@@ -93,12 +93,12 @@ export class BgCanvasComponent implements OnInit {
             // chromatic_aberration: 1.0,
             distortion: .25,
             dof_focus_distance: 4,
-            dof_aperture: 3.5,
+            dof_aperture: 2.0,
             grain_amount: 1.0,
             dof_pentagon: true,
             dof_gain: 1.0,
             dof_threshold: 1.0,
-            dof_darken: 0.2,
+            dof_darken: 0.3,
         }, loadedScene, 1.0, [loadedScene.activeCamera]);
 
     }
